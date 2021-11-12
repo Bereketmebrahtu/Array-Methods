c5d8e052e248d6e931cccebc75468ba839e303eb
Beteywn <mebrahtubereket@gmail.com>
2021-11-12 19:19:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-nvkcd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nvkcd2)
---
+++ index.js
@@ -0,0 +1,14 @@
+let inputEl = document.querySelector('#input').value;
+let buttonEl = document.querySelector('button');
+let outputEl = document.querySelector('p')
+
+const array = [1,2,3,4]
+const sum = 0 
+
+function reducer(total, num) { 
+  return total + num
+
+
+}
+
+//console.log(reducer(array), 2)
