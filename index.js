40f3fba5f3f57ca4267bc6fbfeb9a2345affe86b
Beteywn <mebrahtubereket@gmail.com>
2021-11-15 18:49:31 +0000

    2 files modified

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ let buttonEl = document.querySelector('button');
 let outputEl = document.querySelector('p')
 
 
+const myArray =[];
+function filteredItems(str) { 
+  //const inputText = document.querySelector('#input').value;
+     myArray.push(str)
+     //console.log(myArray)
 
-function filteredItems() { 
- const items = inputEl.filter(value => value /2)
+     let inputVal = "";
+     for(let i =0; i< myArray.length; i++) {
+       inputVal += myArray[i]
+       
+     } 
+        inputVal.split(' ')
+//document.querySelector('#input').innerHTML = inputVal
 
-console.log(items)
 }
-
+//console.log(filteredItems(str))
 outputEl.addEventListener("click", filteredItems)
+console.log(filteredItems("1,2,3"))       
