6a1b0524cd3fc803065f94b2aacf599055ad365c
Beteywn <mebrahtubereket@gmail.com>
2021-11-16 23:43:32 +0000

    1 file modified

--- index.js
+++ index.js
@@ -11,51 +11,25 @@ let outputEl = document.querySelector('p')
 
 
 
-<<<<<<< HEAD
+
  
    
   const filtered = arr => arr.filter(num => num % 2 == 0);
 
-  console.log(filtered([1,2,3))
+  console.log(filtered([1,2,3])
 
      
       
   
 
-   
-   
-  console.log(inputVal)
-
-=======
-
-function filteredItems(arr){
-  //const inputData = document.querySelector('#input');
   
-       let myArray = []
->>>>>>> 3437166df82ca79eef4f0bf9d90202de16a46944
-
-for(let i=0; i< arr.length; i++) {
-       
-       if(arr % 2 === 0) {
-        
-         myArray.push(arr[i])
-         console.log(myArray)
-       }
-       
-        
-       
-      }
-            
-           
- }
 
  
   
   
  
 
-  }
-outputEl.innerHTML = myArray.filter(filteredItems)
+  
 
 
-console.log(filteredItems([1,2,3,4]))
+
